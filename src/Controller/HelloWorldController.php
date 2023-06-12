224c9c0a80acc3cc6ebf017be07be865e36f323c
Adrien Roches <adrien.roches@sensiolabs.com>
2023-06-12 13:59:26 +0200

    [sfstart] Set up hello world first controller

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function strtoupper;
+use function ucfirst;
+
+class HelloWorldController extends AbstractController
+{
+    #[Route(
+        path: '/hello/{name}',
+        name: 'app_hello_world',
+        requirements: ['name' => '\w([-\w])*'],
+        methods: ['GET'],
+    )]
+    public function index(string $name = 'Adrien'): Response
+    {
+        $name = ucfirst($name);
+
+        return new Response(
+            content: <<<"HTML"
+            <body>Hello {$name} !</body>
+            HTML
+        );
+    }
+}
