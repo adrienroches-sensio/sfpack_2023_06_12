243f4ce3548f1d52c9e1af9ecf677643b3151e4c
Adrien Roches <adrien.roches@sensiolabs.com>
2023-06-13 14:05:43 +0200

    [sfstart - form] Create base form for Movie

---
+++ src/Form/MovieType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('slug')
+            ->add('title')
+            ->add('poster')
+            ->add('releasedAt')
+            ->add('plot')
+            ->add('genres')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
