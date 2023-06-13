94f12685cb82fee9a59f1fed7535e11a69e4304c
Adrien Roches <adrien.roches@sensiolabs.com>
2023-06-13 17:09:02 +0200

    [sfstart - test] Add data providers to the smoke test

--- tests/Controller/HelloWorldControllerTest.php
+++ tests/Controller/HelloWorldControllerTest.php
@@ -2,14 +2,35 @@
 
 namespace App\Tests\Controller;
 
+use Generator;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HelloWorldControllerTest extends WebTestCase
 {
-    public function testItWorks(): void
+    public static function provideValidURLs(): Generator
+    {
+        yield 'default' => [
+            'uri' => '/hello',
+        ];
+
+        yield 'with name "Adrien"' => [
+            'uri' => '/hello/Adrien',
+        ];
+
+        yield 'with name "Adrien-Louise"' => [
+            'uri' => '/hello/Adrien-Louise',
+        ];
+    }
+
+    /**
+     * @group type/smoke
+     *
+     * @dataProvider provideValidURLs
+     */
+    public function testItWorks(string $uri): void
     {
         $client = static::createClient();
-        $client->request('GET', '/hello');
+        $client->request('GET', $uri);
 
         $this->assertResponseIsSuccessful();
     }
