ccf86bb5141fecf66c491f99810c23c70718f504
Adrien Roches <adrien.roches@sensiolabs.com>
2023-06-15 10:16:30 +0200

    [sfstart - security] Modify the custom authenticator

--- src/Security/AppFormLoginAuthenticator.php
+++ src/Security/AppFormLoginAuthenticator.php
@@ -21,7 +21,7 @@ class AppFormLoginAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private readonly UrlGeneratorInterface $urlGenerator)
     {
     }
 
@@ -46,9 +46,7 @@ class AppFormLoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
     protected function getLoginUrl(Request $request): string
